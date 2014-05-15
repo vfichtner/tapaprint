98ba44b0c3fb14b331bb27d411f10360e5687e16
sprucewerk <vitalifichtner@vfichtner-mbp.local>
2014-05-15 22:05:11 +0200

    Initial commit

---
+++ imageprocessing.js
@@ -0,0 +1,51 @@
+/**
+ * Created by vitalifichtner on 06.05.14.
+ */
+var im = require('imagemagick');
+
+ImageProcessing = function(){
+
+}
+
+
+ImageProcessing.prototype.resizeToLarge = function(fileName,callback){
+
+    //make a thumbnail
+    im.resize({
+        srcPath: '/data/tapaprint/'+fileName,
+        dstPath: '/data/tapaprint/'+fileName,
+        quality: 0.8,
+        width:   300
+    }, function(err, stdout, stderr){
+        if (err){
+            console.log('error while resize = '+err);
+            callback(err);
+        }
+        callback(null);
+    });
+
+}
+
+
+
+ImageProcessing.prototype.resizeToSmall = function(fileName,callback){
+
+    //make a thumbnail
+    im.resize({
+        srcPath: '/data/tapaprint/'+fileName,
+        dstPath: '/data/tapaprint/small/'+fileName,
+        quality: 0.8,
+        width:   300
+    }, function(err, stdout, stderr){
+        if (err){
+            console.log('error while resize = '+err);
+            callback(err);
+        }
+        callback(null);
+    });
+
+}
+
+
+
+exports.ImageProcessing = ImageProcessing;
---
+++ public/javascripts/photoswipe.js
@@ -0,0 +1,9 @@
+/**
+ * Created by vitalifichtner on 28.04.14.
+ */
+
+alert('huhu');
+$(document).ready(function(){
+    $("#Gallery a").photoSwipe();
+});
+alert('huhu 2');
